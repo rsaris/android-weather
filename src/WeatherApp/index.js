87e761287f7c84825f8c4b0ddd26506c0a6eb059
Bob Saris <bob@bobsaris.com>
2021-01-03 21:40:00 -0500

    Hello world!

---
+++ src/WeatherApp/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  ScrollView,
+  Text,
+} from 'react-native';
+
+export default function WeatherApp() {
+  return (
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+    >
+      <Text>Hello world!</Text>
+    </ScrollView>
+   );
+}
